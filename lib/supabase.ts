1dd4de4149c400dfc79fae9fdd3dc121c1537fa0
benmneb <62878664+benmneb@users.noreply.github.com>
2023-05-07 11:15:20 +0800

    fix: use standard deno dotenv module

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,4 @@
-import { config } from "dotenv/mod.ts";
 import { createClient } from "supabase";
-
-config({ safe: true, export: true });
 
 const SUPABASE_URL = String(Deno.env.get("SUPABASE_URL"));
 const SUPABASE_KEY = String(Deno.env.get("SUPABASE_KEY"));
