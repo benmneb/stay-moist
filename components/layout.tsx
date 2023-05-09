393ace719e6713e5fceca883bf3a194e7dce5c8a
benmneb <62878664+benmneb@users.noreply.github.com>
2023-05-09 21:31:19 +0800

    fix: alignment of login button

--- components/layout.tsx
+++ components/layout.tsx
@@ -47,7 +47,7 @@ export function Layout({ children, state, title }: Props) {
 							</a>
 						</>
 					) : (
-						<a href="/log-in" class="flex flex-row items-center gap-2">
+						<a href="/log-in" class="flex flex-row items-center gap-2 -mr-2">
 							<button
 								type="button"
 								class="px-3 py-2 bg-transparent rounded hover:bg-gradient-to-r from-indigo-100 via-violet-100 to-purple-100 flex gap-2"
