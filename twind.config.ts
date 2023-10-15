badcfd807aec4655a0abf9359c6c1ec117541822
benmneb <62878664+benmneb@users.noreply.github.com>
2023-10-15 11:54:11 +0800

    feat: add twind line-clamp preset

--- twind.config.ts
+++ twind.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from "twind";
-import { Options } from "twind_fresh_plugin/twind.ts";
+import { defineConfig } from 'twind'
+import { Options } from 'twind_fresh_plugin/twind.ts'
 // twind preset
-import presetAutoPrefix from "twind-preset-autoprefix";
-import presetTailWind from "twind-preset-tailwind";
+import presetAutoPrefix from 'twind-preset-autoprefix'
+import presetLineClamp from 'twind-preset-line-clamp'
+import presetTailWind from 'twind-preset-tailwind'
 
 export default {
-  ...defineConfig({
-    presets: [presetAutoPrefix(), presetTailWind()],
-    theme: {
-      extend: {
-        boxShadow: {
-          "next": "0 4px 14px 0 rgba(0, 0, 0, 0.3)",
-        },
-      },
-    },
-  }),
-  selfURL: import.meta.url,
-} as unknown as Options;
+	...defineConfig({
+		presets: [presetAutoPrefix(), presetTailWind(), presetLineClamp()],
+		theme: {
+			extend: {
+				boxShadow: {
+					next: '0 4px 14px 0 rgba(0, 0, 0, 0.3)',
+				},
+			},
+		},
+	}),
+	selfURL: import.meta.url,
+} as unknown as Options
